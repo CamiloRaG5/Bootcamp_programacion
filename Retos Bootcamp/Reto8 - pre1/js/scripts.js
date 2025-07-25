b148c919423f9ab21686f469a0c26a97535fad37
CamiloRaG5 <camilo.ramirezg@udea.edu.co>
2025-07-25 12:15:57 -0500

    Reto 8 - pre1

---
+++ Retos Bootcamp/Reto8 - pre1/js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // Obtener la referencia de los documentos de la pagina
+    const inputBox = document.getElementById('inputBox');
+    const showBtn = document.getElementById('showBtn');
+    const outputBox = document.getElementById('outputBox');
+    // Añadir un observador al boton
+    showBtn.addEventListener('click', function(){
+        const message = inputBox.value;
+        console.log(message);
+    })
+})
